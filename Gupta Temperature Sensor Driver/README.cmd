4afc94fac9211f31ccbc9e3342197462021f536d
sgupta999 <35128032+sgupta999@users.noreply.github.com>
2019-09-09 13:15:15 -0500

    Update README.cmd

--- Gupta Temperature Sensor Driver/README.cmd
+++ Gupta Temperature Sensor Driver/README.cmd
@@ -1,7 +1,11 @@
 Gupta Temperature Sensor Driver SmartApp for Smartthings
 ===========================================================
 
-Can be used with any Smartthings device that has a temperature sensor capability - Temp Sensor(s), Thermostat(s) Converts a basic temperature sensor to a thermostat like device. You can set up Heating and Cooling setpoints and variances so you can control smartthing devices when CSP and HSP on/off events are triggered. I also added contact sensors so you can trigger virtual sensors to pass these events to Alexa.
+Can be used with any Smartthings device that has a temperature sensor capability - Temp Sensor(s), Thermostat(s) 
+
+Converts a basic temperature sensor to a thermostat like device. You can set up Heating and Cooling setpoints and variances so you can control smartthing devices when CSP and HSP on/off events are triggered. 
+
+I also added contact sensors so you can trigger virtual sensors to pass these events to Alexa.
 
 Please let me know if you want to add any other types of device types but right now pretty basis On/Off, Open/Close functionality.
 
